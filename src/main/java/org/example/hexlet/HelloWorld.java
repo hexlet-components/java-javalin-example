6145d460bd6ebd48aa24c2a9b04e0126e158fc49
s-chepurnov <Kitaec_31337>
2023-12-16 11:17:46 +0700

    Универсальный метод считывания файла из ресурсной директории

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
